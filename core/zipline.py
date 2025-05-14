8cb12d5952fb9c214118d0105dce26b379228247
CaldeiraG <cal@caldeirag.xyz>
2025-05-14 12:42:14 +0100

    hopefully fixed

--- core/zipline.py
+++ core/zipline.py
@@ -1,29 +1,26 @@
 from .config import config
-from PIL import Image
 from typing import Optional
 from utils.logging import logger
-import models.zipline
-import io
 import requests
 
 def uploadToZipline(url: str) -> Optional[str]:
     try:
-        logger.debug(url)
-        originalImageBytesIO = io.BytesIO(requests.get(url).content)
-        originalImage = Image.open(originalImageBytesIO).convert("RGB")
-        newImage = Image.new("RGB", originalImage.size)
-        newImage.putdata(originalImage.getdata()) # pyright: ignore[reportArgumentType]
-        newImageBytesIO = io.BytesIO()
-        newImage.save(newImageBytesIO, subsampling = 0, quality = 90, format = "JPEG")
+        originalImage = requests.get(url).content
+        filename = url.split("/")[-1] 
+        #originalImageBytesIO = io.BytesIO(requests.get(url).content)
+        #originalImage = Image.open(originalImageBytesIO).convert("RGB")
+        #newImage = Image.new("RGB", originalImage.size)
+        #newImage.putdata(originalImage.getdata()) # pyright: ignore[reportArgumentType]
+        #newImageBytesIO = io.BytesIO()
+        #newImage.save(newImageBytesIO, subsampling = 0, quality = 90, format = "JPEG")
         response = requests.post(
-			"https://share.caldeirag.xyz/api/upload",
-			headers = { "Authorization": f"{config['display']['posters']['ziplineAuth']}", "Content-Type": "multipart/form-data" },
-			files = { "file": newImageBytesIO.getvalue() }
-		)
+            "https://share.caldeirag.xyz/api/upload",
+            headers = { "Authorization": f"{config['display']['posters']['ziplineAuth']}"},
+            files = {"file" : (filename,originalImage, "image/jpeg") })
         logger.debug("HTTP %d, %s, %s", response.status_code, response.headers, response.text.strip())
-        data: models.zipline.UploadResponse = response.json()
-        if not data["success"]:
-        	raise Exception(data["data"]["error"])
-        return data["data"]["url"]
+        data = response.json()
+    	if not response.status_code == 200:
+        	raise Exception(data["files"]["error"])
+        return data["files"][0]["url"]
     except:
         logger.exception("An unexpected error occured while uploading an image to Zipline")
